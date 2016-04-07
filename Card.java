e653f27f18d0cd6bd9fa48dca1cdf10109446570
Yates <yates.monteith@gmail.com>
2016-04-07 11:38:12 -0400

    First steps on part 3

--- Card.java
+++ Card.java
@@ -1,24 +1,17 @@
 package com.energyhub.interview.setgame;
 
+import java.util.*;
+
 public class Card implements Comparable<Card> {
-	private final Color color;
-	private final Shading shading;
-	private final Symbol symbol;
-	private final Integer number;
-	
+	private final Map<String, String> descriptorMap;
+
 	/**
 	 * Generates a card given the color, shading, symbol and number on the card.
 	 *
-	 * @param color color of the symbols on the card
-	 * @param shading shading of the color on the card
-	 * @param symbol symbol on the card
-	 * @param number number of symbols on the card
 	 */
-	public Card(Color color, Shading shading, Symbol symbol, int number) {
-		this.color = color;
-		this.shading = shading;
-		this.symbol = symbol;
-		this.number = new Integer(number);
+	public Card(Map<String, String> descriptorMap) {
+		this.descriptorMap = new HashMap<String, String>();
+		this.descriptorMap.putAll(descriptorMap);
 	}
 
 	/**
@@ -28,37 +21,31 @@ public class Card implements Comparable<Card> {
      */
 	@Override
 	public String toString() {
-		String s = number + " " + shading + " " + color + " " + symbol;
-		if(number > 1)
-			s = s + "S";
+		List<String> descriptorList = new ArrayList<String>();
+		descriptorList.addAll(descriptorMap.keySet());
+		Collections.sort(descriptorList);
+		String s = "";
+		for(String descriptor : descriptorList) {
+			s += descriptorMap.get(descriptor) + " ";
+		}
 		return s;
-				
 	}
-	
-	/** Getters **/
-	
-	public Color getColor() {
-		return color;
-	}
-	
-	public Shading getShading() {
-		return shading;
-	}
-	
-	public Symbol getSymbol() {
-		return symbol;
-	}
-	
-	public Integer getNumber() {
-		return number;
+
+	public String getDescriptor(String descriptor) {
+		return descriptorMap.get(descriptor);
 	}
 
 	public int compareTo(Card c) {
 		int returnValue = 0;
-		if(!this.color.equals(c.color) || !this.symbol.equals(c.symbol) || !this.shading.equals(c.shading) ||
-				!this.number.equals(c.number))
-			returnValue = -1;
+		List<String> descriptorList = new ArrayList<String>();
+		descriptorList.addAll(descriptorMap.keySet());
+		Collections.sort(descriptorList);
+		for(String descriptor : descriptorList) {
+			if(this.descriptorMap.get(descriptor).compareTo(c.descriptorMap.get(descriptor)) != 0) {
+				returnValue = -1;
+				break;
+			}
+		}
 		return returnValue;
-
 	}
 }
